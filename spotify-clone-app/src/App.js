4bd19790d284286eb5fe731894d34c2d5cfebc00
wahidul-dev <wahidul.abir@northsouth.edu>
2020-12-21 11:42:29 +0600

    token read feature added

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log("I have A token>> ", token);
+  }, []);
+
   return (
     // BEM
     <div className="app">
-      <h1>Hi</h1>
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
