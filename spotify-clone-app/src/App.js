98d605ed35382ff2bbd6a272a8c335452c31f9da
wahidul-dev <wahidul.abir@northsouth.edu>
2020-12-23 01:12:37 +0600

    data layer created

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
 
 const spotify = new SpotifyWebApi();
 
@@ -32,7 +33,7 @@ function App() {
     <div className="app">
       {
         token ? (
-          <h1>I am logged in</h1>
+          <Player />
         ) : (
           <Login />
         )
