35ffd9dda93c2f9eecee36def83678b336951521
wahidul-dev <wahidul.abir@northsouth.edu>
2020-12-23 22:09:47 +0600

    player structure added

--- spotify-clone-app/src/Player.js
+++ spotify-clone-app/src/Player.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./Player.css";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+import Footer from "./Footer";
 
 function Player({ spotify }) {
     return (
         <div className="player">
-            <h1>Welcome to spotify</h1>
+            <div className="player__body">
+                <Sidebar />
+                <Body />
+            </div>
+
+            <Footer />
         </div>
     )
 }
